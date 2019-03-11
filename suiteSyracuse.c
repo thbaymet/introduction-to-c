559b5e66dc0309b8458db6ac3e6d5903697c1ffb
thbaymet <bayramov.matin@gmail.com>
2019-03-11 07:37:52 +0100

    Added 2nd version of syracuse in order to print the index of the last element.

--- suiteSyracuse.c
+++ suiteSyracuse.c
@@ -3,6 +3,8 @@
 //
 // Created by h on 11/03/19.
 //
+int syracuseV1(int k);
+int syracuseV2(int k);
 
 int main() {
     printf("Bienvenue dans le programme d'affichage de suite de Syracuse\n");
@@ -11,7 +13,16 @@ int main() {
     int k = 5;
     scanf("%d", &k);
 
-    int n = 0;
+    syracuseV1(k);
+    printf("\n");
+    syracuseV2(k);
+
+    return 0;
+}
+
+
+int syracuseV1(int k) {
+
     int u = k;
 
     while (u != 1) {
@@ -23,6 +34,22 @@ int main() {
         }
     }
     printf("%d ", u);
+}
 
-    return 0;
+int syracuseV2(int k) {
+
+    int u = k;
+    int n = 0;
+
+    while (u != 1) {
+        printf("u(%d)=%d ", n, u);
+        if (u % 2 == 0) {
+            u = (int)(u / 2);
+        } else {
+            u = (int)(3 * u) + 1;
+        }
+        n++;
+    }
+    printf("u(%d)=%d ", n, u);
+    printf("\nL'indice du premier terme de la suite égal à 1: %d", n);
 }
